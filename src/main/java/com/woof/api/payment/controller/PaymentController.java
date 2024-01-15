7fd0f62e08eb9105be4f59b4780e00967b672a4e
ddarahakit2023 <ddarahakit2023@gmail.com>
2024-01-15 13:04:52 +0900

    [Feat] 결제 기능 추가
    포트원 설정 빈, 컨트롤러, 레포지토리, 엔티티 기본 생성까지 작업

---
+++ src/main/java/com/woof/api/payment/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.woof.api.payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/payment")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class PaymentController {
+
+
+}
