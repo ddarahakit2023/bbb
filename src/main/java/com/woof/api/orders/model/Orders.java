0c163a64d0467b763b2d5b9dc72e8fbffbb9b321
kangmoonhye <ansgp0323@naver.com>
2024-01-16 20:22:22 +0900

    [Feat] 코드 수정

--- src/main/java/com/woof/api/orders/model/Orders.java
+++ src/main/java/com/woof/api/orders/model/Orders.java
@@ -3,10 +3,7 @@ package com.woof.api.orders.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -25,24 +22,27 @@ public class Orders { //예약어 때문에 orders로 했어용
     private String reservation_status; //예약 상태
     private String orderDetails;
 
-//    private String ceo_id;
-//    private String user_id;
-//    private String woof_manager_number;
 
 
-//    //사용자 1 : 주문 N
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "Member_idx")
-//    private Ceo ceo;
+    //업체 1 : 주문 N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_idx")
+    private Ceo ceo;
 //
-//    //상품 1 : 주문 N
+    //매니저 1 : 주문 N
 //    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "Product_idx")
+//    @JoinColumn(name = "product_idx")
 //    private ProductCeo productCeo;
 
-//    //상세아이디 1 : 주문 N
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = )
+    //고객 1 : 주문 N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_idx")
+    private Member member;
+
+    //결제 1 : 주문 N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_idx")
+    private Payment payment;
 
 
 
