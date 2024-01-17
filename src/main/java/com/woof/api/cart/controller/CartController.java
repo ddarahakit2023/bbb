493731d92baaa9a989e0abe40383a21413c5d1e8
heueun <kjh980615@naver.com>
2024-01-17 12:12:12 +0900

    [Fix] 오류수정

--- src/main/java/com/woof/api/cart/controller/CartController.java
+++ src/main/java/com/woof/api/cart/controller/CartController.java
@@ -1,13 +1,17 @@
 package com.woof.api.cart.controller;
 
 
-import com.woof.api.cart.model.dto.CartCreateReq;
+
+
+import com.woof.api.cart.model.Cart;
 import com.woof.api.cart.service.CartService;
 import com.woof.api.member.model.entity.Member;
+import com.woof.api.productManager.model.ProductManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/cart")
@@ -17,21 +21,23 @@ public class CartController {
     private final CartService cartService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/in")
-    public ResponseEntity create(@AuthenticationPrincipal Member member, @RequestBody CartCreateReq cartCreateReq) {
-                             // Member의 UserDetails 객체에 접근가능
-        cartService.create(member, cartCreateReq);
+    public ResponseEntity create(Long productCeoIdx, Long productManagerIdx, String email) {
+
+        cartService.cartIn(productCeoIdx, productManagerIdx, email);
         return ResponseEntity.ok().body("ok");
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/list")
-    public ResponseEntity list(@AuthenticationPrincipal Member member) {
+    @RequestMapping(method = RequestMethod.GET, value = "/cartlist")
+    public ResponseEntity<Object> cartList(/*@AuthenticationPrincipal*/ String email) {
 
-        return ResponseEntity.ok().body(cartService.list(member));
+        return ResponseEntity.ok().body(cartService.cartList(email));
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/cancel/{idx}")
-    public ResponseEntity remove(@AuthenticationPrincipal Member member,@PathVariable Long idx) {
-        cartService.remove(member, idx);
+
+    // Ceo랑 매니저 아이디 따로 해야하나?
+    @RequestMapping(method = RequestMethod.DELETE, value = "/cartremove/{idx}")
+    public ResponseEntity cartRemove(/*@AuthenticationPrincipal*/@PathVariable Long idx) {
+        cartService.cartRemove(idx);
         return ResponseEntity.ok().body("ok");
     }
 }
