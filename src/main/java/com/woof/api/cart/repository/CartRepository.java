3e58fac51979f15db8b1fbece1e702df5c31ebae
heueun <kjh980615@naver.com>
2024-01-31 21:02:39 +0900

    [Feat]  N+1 해결

--- src/main/java/com/woof/api/cart/repository/CartRepository.java
+++ src/main/java/com/woof/api/cart/repository/CartRepository.java
@@ -11,9 +11,5 @@ import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    //fetch join을 써서 해결을 해야한다
-//    @Query("SELECT c FROM CART c " +
-//            "JOIN FETCH c.member JOIN FETCH c.productCeo JOIN FETCH c.productManager " +
-//            "WHERE c.member.idx = :memberIdx")
     List<Cart> findAllByMemberIdx(Long memberIdx);
 }
